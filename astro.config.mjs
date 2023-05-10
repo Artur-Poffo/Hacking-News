1dd4a35023a05817149d70ec5308db9df272eb9a
Artur Poffo <arturpoffoc@hotmail.com>
2023-05-09 23:03:37 -0300

    Reset Tests of Hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'server',
-  adapter: netlify()
 });
